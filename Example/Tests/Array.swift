9eb50e594f219b5d5f381635cf653870e8e7a56b
Charlie Fish <fishcharlie@me.com>
2017-12-13 17:27:20 -0700

    Adding first randomElement test

---
+++ Example/Tests/Array.swift
@@ -0,0 +1,29 @@
+//
+//  Array.swift
+//  utilswift_Tests
+//
+//  Created by Charlie Fish on 12/13/17.
+//
+
+import Quick
+import Nimble
+import utilswift
+
+class ArrayTests: QuickSpec {
+	override func spec() {
+		
+		describe("Array") {
+			context("Number") {
+				var array: [Int] = [Int]()
+				beforeEach {
+					array = [1, 2, 3, 4, 5]
+				}
+				it("Should return number in array") {
+					expect(array).to(contain(array.randomElement()))
+				}
+			}
+		}
+		
+	}
+}
+
